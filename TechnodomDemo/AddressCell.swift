13350c93bf074cb51b72ecbf93f068e27cf3460f
Yerlan Ismailov <y.ismailov@infin-it.kz>
2020-08-10 19:53:13 +0600

    implement ui for Address cell

---
+++ TechnodomDemo/AddressCell.swift
@@ -0,0 +1,13 @@
+//
+//  AddressCell.swift
+//  TechnodomDemo
+//
+//  Created by Yerlan Ismailov on 8/10/20.
+//  Copyright © 2020 ismailov.kz. All rights reserved.
+//
+
+import UIKit
+
+class AddressCell: UICollectionViewCell {
+    @IBOutlet weak var addressLabel: UILabel!
+}
