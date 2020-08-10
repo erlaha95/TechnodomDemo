451e1f66ee87c851c1ee49b7fdc290e44ef50f0c
Yerlan Ismailov <y.ismailov@infin-it.kz>
2020-08-10 19:47:24 +0600

    initial commit

---
+++ TechnodomDemo/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  TechnodomDemo
+//
+//  Created by Yerlan Ismailov on 8/10/20.
+//  Copyright © 2020 ismailov.kz. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+
+}
+
