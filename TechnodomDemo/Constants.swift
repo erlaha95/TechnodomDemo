0606f565cf1ce635f746f3358fb56e396cf074e7
Yerlan Ismailov <y.ismailov@infin-it.kz>
2020-08-10 19:49:00 +0600

    create Constants struct to access project constants

---
+++ TechnodomDemo/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  TechnodomDemo
+//
+//  Created by Yerlan Ismailov on 8/10/20.
+//  Copyright © 2020 ismailov.kz. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct UI {
+        static let defaultCornerRadius = 16.0
+    }
+    
+}
